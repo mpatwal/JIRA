ef98d7abf1fd99a0a53fec57c1dd4662d59acaac
Manisha Patwal <manishapatwal34@gmail.com>
2025-07-03 18:09:06 +0530

    updated

--- JIRA_Backend/src/main/java/com/manisha/jira/Security/SecurityConfig.java
+++ JIRA_Backend/src/main/java/com/manisha/jira/Security/SecurityConfig.java
@@ -1,13 +1,31 @@
 package com.manisha.jira.Security;
-
+import com.manisha.jira.service.JWTFilter;
+import com.manisha.jira.service.UserDetailServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
+    private final UserDetailServiceImpl userDetailsService;
+    @Autowired private JWTFilter jwtFilter;
+
+    public SecurityConfig(UserDetailServiceImpl userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -19,5 +37,23 @@ public class SecurityConfig {
                 );
 
         return http.build();
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config)
+            throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
     }
 }
--- JIRA_Backend/src/main/java/com/manisha/jira/controller/AuthController.java
+++ JIRA_Backend/src/main/java/com/manisha/jira/controller/AuthController.java
@@ -1,50 +1,57 @@
 package com.manisha.jira.controller;
 
+import com.manisha.jira.dto.AuthRequest;
+import com.manisha.jira.dto.AuthResponse;
 import com.manisha.jira.dto.RegisterUser;
 import com.manisha.jira.entity.User;
 import com.manisha.jira.repository.UserRepository;
 import com.manisha.jira.service.UserDetailServiceImpl;
 import com.manisha.jira.util.JWTUtil;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("/api/auth")
+
 public class AuthController {
-//    @Autowired
-//    private AuthenticationManager authenticationManager;
+    @Autowired private AuthenticationManager authenticationManager;
+    @Autowired private JWTUtil jwtUtil;
+    @Autowired private UserDetailServiceImpl userDetailService;
+    @Autowired private  UserRepository userRepository;
+    @Autowired private  PasswordEncoder passwordEncoder;
 
-    @Autowired
-    private JWTUtil jwtUtil;
 
-    @Autowired
-    private UserDetailServiceImpl userDetailServices;
-
-    @Autowired
-    private UserRepository userRepository;
-
-//    @Autowired
-//    private PasswordEncoder passwordEncoder;
-
-    @PostMapping("/register")
-    public ResponseEntity<String> register(@RequestBody RegisterUser registerUser) {
+@CrossOrigin
+@PostMapping("/register")
+public ResponseEntity<String> register(@RequestBody RegisterUser request) {
         User user = User.builder()
-                .username(registerUser.getUsername())
-                .email(registerUser.getEmail())
-                .password(registerUser.getPassword())
-                .role(registerUser.getRole())
+                .username(request.getUsername())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(request.getRole())
                 .createdAt(LocalDateTime.now())
                 .build();
         userRepository.save(user);
-        return ResponseEntity.ok("User registered");
+        return ResponseEntity.ok("User registered...");
+    }
+@CrossOrigin
+@PostMapping("/login")
+public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest authRequest) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(authRequest.getUsername(),
+                        authRequest.getPassword())
+                );
+        UserDetails userDetails = userDetailService.loadUserByUsername(authRequest.getUsername());
+        String token = jwtUtil.generateToken(userDetails);
+        return ResponseEntity.ok(new AuthResponse(token));
     }
 }
